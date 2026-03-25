ba188f2514f431996895c18554be505369478e8f
houseofantiques <houseofantique30@gmail.com>
2026-03-26 01:44:45 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+import { ARTWORKS_PRODUCTS } from "./artworks.js";
+import { PAINTINGS_PRODUCTS } from "./paintings.js";
+import { WOOD_PRODUCTS } from "./wood.js";
+import { COPPER_PRODUCTS } from "./copper.js";
+import { SILVER_PRODUCTS } from "./silver.js";
+import { CRYSTAL_PRODUCTS } from "./crystal.js";
+import { FURNITURE_PRODUCTS } from "./furniture.js";
+import { ARABIC_CALLIGRAPHY_PRODUCTS } from "./ARABIC_CALLIGRAPHY.js";
+import { ACCESSORIES_PRODUCTS } from "./Accessories.js";
+import { CARPETS_PRODUCTS } from "./carpets.js";
+import { VASES_PRODUCTS } from "./vases.js";
+
+window.PRODUCTS = [
+  ...ARTWORKS_PRODUCTS,
+  ...PAINTINGS_PRODUCTS,
+  ...WOOD_PRODUCTS,
+  ...COPPER_PRODUCTS,
+  ...SILVER_PRODUCTS,
+  ...CRYSTAL_PRODUCTS,
+  ...FURNITURE_PRODUCTS,
+  ...ARABIC_CALLIGRAPHY_PRODUCTS,
+  ...ACCESSORIES_PRODUCTS,
+  ...CARPETS_PRODUCTS,
+  ...VASES_PRODUCTS,
+];
